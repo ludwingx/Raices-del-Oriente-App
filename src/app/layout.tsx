a8cee5ed5406a8529f23d58e77d4d0d3e766897b
ludwingx <ludwingytx@gmail.com>
2024-10-24 09:39:58 -0400

    changed rute for import to global.css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "./globals.css";
 import Navbar from "@/components/nav";
+import '@/app/globals.css';
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
